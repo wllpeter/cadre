c94dfdfe338348974b3348724cfb107bc0f52737
fuming <fm365482>
2017-10-20 18:33:28 +0800

    家庭成员信息枚举类

--- xaplan-cadre/src/main/java/com/ddb/xaplan/cadre/enums/FamilyRelationship.java
+++ xaplan-cadre/src/main/java/com/ddb/xaplan/cadre/enums/FamilyRelationship.java
@@ -2,11 +2,24 @@ package com.ddb.xaplan.cadre.enums;
 
 /**
  * Created by 陈亚兰 on 2017/10/17.
- * 家庭关系,依次分别是父亲，母亲，孩子，兄弟姐妹
+ * 家庭关系,依次分别是父亲，母亲，儿子,女儿，兄弟，姐妹,户主,配偶,儿媳,女婿,外孙女,外孙子,孙子，孙女,祖母，祖父
  */
 public enum  FamilyRelationship {
     FATHER,
     MOTHER,
-    CHILD,
-    BROTHERORSISTER
+    SON,
+    DAUGHTER,
+    BROTHEROR,
+    SISTER,
+    HOUSEHOLDER,
+    SPOUSE,
+    SONWIFE,
+    DAUGHTERHUSBAND,
+    GRANDDAUGHTER,
+    GRANDSON,
+    SONSON,
+    SONDAUGHTER,
+    GRANDMOTHER,
+    GRANDFATHER
+
 }
