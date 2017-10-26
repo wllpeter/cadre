d4a6052d27162438c1c5d68769a1b6bac1c4b48f
fuming <fm365482>
2017-10-26 14:43:28 +0800

    修改首页接口访问路径

--- xaplan-cadre/src/main/java/com/ddb/xaplan/cadre/controller/OfficerIllegalInfoController.java
+++ xaplan-cadre/src/main/java/com/ddb/xaplan/cadre/controller/OfficerIllegalInfoController.java
@@ -8,10 +8,7 @@ import com.ddb.xaplan.cadre.service.OfficerIllegalInfoService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -56,8 +53,8 @@ public class OfficerIllegalInfoController {
     @ApiOperation(value = "search IllegalStatistics info controller")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "areaName",paramType = "PathVariable", dataType = "String") })
-    @GetMapping(value="/{areaName}/getIllegalStatistics")
-    public DataInfo<Map<String,List>> getCrimeStatistics(@PathVariable String areaName){
+    @GetMapping(value="/getIllegalStatistics")
+    public DataInfo<Map<String,List>> getCrimeStatistics(@RequestParam("areaName") String areaName){
         int areaId=0;
         if(areaName.equals("雄县")){
             areaId=3;
@@ -68,7 +65,7 @@ public class OfficerIllegalInfoController {
         }else{
             areaId=0;
         }
-        Map<String,List> map=new HashMap<String,List>();
+            Map<String,List> map=new HashMap<String,List>();
         List<StatisticsBean> yearList=this.officerIllegalInfoService.getGroupYearIllegalCount(areaId);
         List<StatisticsBean> punishmentList=this.officerIllegalInfoService.getGroupPunishmentIllegalCount(areaId);
         List<StatisticsBean> briefList=this.officerIllegalInfoService.getGroupBriefIllegalCount(areaId);
